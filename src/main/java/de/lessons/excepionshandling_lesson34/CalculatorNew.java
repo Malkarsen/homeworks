f23c12798659960754b6fdfdcd405fe268f9c4a2
Arina Malko <malko.arina212@gmail.com>
2025-01-29 18:14:03 +0100

    Implementing BadError, Calculator classes to study exceptions

---
+++ src/main/java/de/lessons/excepionshandling_lesson34/CalculatorNew.java
@@ -0,0 +1,14 @@
+package de.lessons.excepionshandling_lesson34;
+
+public class CalculatorNew {
+    public static void main(String[] args) {
+        try {
+            Calculator.divideNumbersThrowsException(20, 10);
+        } catch (ArithmeticException exception) {
+            System.out.println("Я делил на 0 !!!! Это моя ошибка !!!");
+            exception.getStackTrace();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
